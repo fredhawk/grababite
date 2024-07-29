d712cc172f39e608c901fb706884b2491d3f0f22
Fred Hawk <fredrichawk@gmail.com>
2024-07-29 20:43:21 +0200

    fix: Connection string exchanged for url as that is the new param.

--- src/lib/server/drizzle.ts
+++ src/lib/server/drizzle.ts
@@ -3,7 +3,7 @@ import postgres from 'postgres';
 
 import config from '../../../drizzle.config';
 
-export const client = postgres(config.dbCredentials.connectionString);
+export const client = postgres(config.dbCredentials.url);
 export const db = drizzle(client, {
 	schema: {
 		// Monster,
